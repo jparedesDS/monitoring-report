b1943592cbdab01fa2a39d3a88bd776c624a92a0
Jose Paredes Colmenarejo <93651084+jparedesDS@users.noreply.github.com>
2024-07-12 09:49:22 +0200

    add critics and new graphics

--- monitoring_report.py
+++ monitoring_report.py
@@ -17,13 +17,14 @@ start_time = time.time()
 path_pending = 'C:\\Users\\alejandro.berzal\\Desktop\\DATA SCIENCE\\monitoring_report\\data_import\\pending.xlsx'
 path_under_review = 'C:\\Users\\alejandro.berzal\\Desktop\\DATA SCIENCE\\monitoring_report\\data_import\\under_review.xlsx'
 path_to_upload = 'C:\\Users\\alejandro.berzal\\Desktop\\DATA SCIENCE\\monitoring_report\\data_import\\to_upload.xlsx'
-path_to_graphics = 'C:\\Users\\alejandro.berzal\\Desktop\\DATA SCIENCE\\monitoring_report\\data_import\\total.xlsx'
+path_total = 'C:\\Users\\alejandro.berzal\\Desktop\\DATA SCIENCE\\monitoring_report\\data_import\\total.xlsx'
 # Leer el DataFrame desde una hoja existente
 df = pd.read_excel(path_pending)
 df2 = pd.read_excel(path_under_review)
 df3 = pd.read_excel(path_to_upload)
-df5 = pd.read_excel(path_to_graphics)
-df6 = pd.read_excel(path_to_graphics)
+df4 = pd.read_excel(path_total)
+df5 = pd.read_excel(path_total)
+df6 = pd.read_excel(path_total)
 
 # TRATAMIENTO DEL DATAFRAME "PENDING / df"
 today_date = pd.to_datetime('today', format='%d-%m-%Y', dayfirst=True)  # Capturamos la fecha actual del día
@@ -95,8 +96,29 @@ df3['Fecha'] = df3['Fecha'].dt.date
 df3['Fecha Prevista'] = df3['Fecha Prevista'].dt.date
 df3['Fecha Pedido'] = df3['Fecha Pedido'].dt.date
 critics_no = df3[df3['Crítico'] == 'No'] # Filtrar los documentos que tienen 'No' en la columna 'Crítico'
-critics_si = df3[df3['Crítico'] == 'Sí'] # Filtrar los documentos que tienen 'Sí' en la columna 'Crítico'
 print(df3)
+
+# TRATAMIENTO DEL DATAFRAME "CRÍTICOS"
+# Transformamos todas las columnas de fechas to_datetime
+df4['Fecha'] = pd.to_datetime(df4['Fecha'], format="%d-%m-%Y", dayfirst=True)
+df4['Fecha Pedido'] = pd.to_datetime(df4['Fecha Pedido'], format="%d-%m-%Y", dayfirst=True)
+df4['Fecha Prevista'] = pd.to_datetime(df4['Fecha Prevista'], format="%d-%m-%Y", dayfirst=True)
+df4['Estado'] = df4['Estado'].fillna('Sin Enviar') # Completamos la columna 'Estado' con 'Sin Enviar'
+df4 = df4[df4['Estado'] != 'Eliminado']
+df4 = df4[df4['Estado'] != 'Aprobado']
+df4 = df4[df4['Estado'] != 'Enviado']
+df4.insert(14, "Días Devolución", (today_date - df4['Fecha']).dt.days) # Insertar nueva columna 'Días Devolución' y restamos a la fecha actual para que nos de el total de días
+# Añadimos la columna 'Fecha Contractual'
+df4.insert(15, 'Fecha Contractual', ((df4['Fecha Prevista'] - df4['Fecha Pedido']).dt.days // 7))
+df4['Fecha Contractual'] = "Aprobación + " + df4['Fecha Contractual'].astype(str) + ' Semanas'
+apply_responsable(df4)
+identificar_cliente_por_PO(df4) # Aplicar el mapping para cambiar el tipo de 'Cliente'
+# Transformamos todas las fechas al formato 'DIA-MES-AÑO' sin la hora
+df4['Fecha'] = df4['Fecha'].dt.date
+df4['Fecha Prevista'] = df4['Fecha Prevista'].dt.date
+df4['Fecha Pedido'] = df4['Fecha Pedido'].dt.date
+critics_si = df4[df4['Crítico'] == 'Sí'] # Filtrar los documentos que tienen 'Sí' en la columna 'Crítico'
+print(df4)
 
 # TRATAMIENTO DEL DATAFRAME "GRÁFICOS / df5"
 df5['Estado'] = df5['Estado'].fillna('Sin Enviar') # Completamos la columna 'Estado' con 'Sin Enviar'
@@ -117,6 +139,9 @@ suma_total_general = df5['Aprobado']
 porcentaje_total = (suma_total_general / suma_total) * 100
 df5['% Completado'] = porcentaje_total
 df5 = df5.reindex(columns=['Nº Pedido', '% Completado', 'Aprobado', 'Com. Mayores', 'Com. Menores', 'Enviado', 'Rechazado', 'Sin Enviar', 'Total'])
+# Ordenar los datos por una columna específica en orden descendente (de Z a A)
+columna_para_ordenar = 'Nº Pedido'  # Reemplaza con el nombre de tu columna
+df5 = df5.sort_values(by=columna_para_ordenar, ascending=False)
 print(df5)
 print("Generando porcentaje total de los pedidos...")
 
@@ -146,98 +171,13 @@ df6['Fecha Pedido'] = df6['Fecha Pedido'].dt.date
 df6['Fecha AP VDDL'] = df6['Fecha AP VDDL'].dt.date
 print(df6)
 
-# Leer la hoja "DATA" usando pandas para análisis
-df_cal_pla = pd.read_excel(path_to_graphics)
-# Filtrar por 'Tipo Doc.'
-df_cal_pla = df_cal_pla[df_cal_pla['Tipo Doc.'] == 'Cálculo y plano']
-# Rellenar valores nulos en 'Estado' con 'Sin Enviar'
-df_cal_pla['Estado'] = df_cal_pla['Estado'].fillna('Sin Enviar')
-# Reemplazar NaN en 'Nº Doc. Cliente' con 'Sin TR. Number'
-df_cal_pla['Nº Doc. Cliente'] = df_cal_pla['Nº Doc. Cliente'].fillna('Sin TR. Number')
-# Concatenar 'Nº Doc. Cliente' en caso de duplicados por 'Nº Pedido'
-df_cal_pla['Nº Doc. Cliente'] = df_cal_pla.groupby('Nº Pedido')['Nº Doc. Cliente'].transform(lambda x: ';'.join(x.astype(str).unique()))
-# Eliminar duplicados manteniendo solo la primera ocurrencia de cada 'Nº Pedido'
-df_cal_pla = df_cal_pla.drop_duplicates(subset=['Nº Pedido', 'Estado'])
-# Contar la frecuencia de cada estado por 'Nº Pedido'
-df_cal_pla = df_cal_pla.groupby(['Nº Pedido', 'Nº Doc. Cliente', 'Estado']).size().unstack(fill_value=0).reset_index()
-# Calcular el total y el porcentaje completado
-df_cal_pla['Total'] = df_cal_pla.iloc[:, 2:].sum(axis=1)
-suma_total = df_cal_pla['Total']
-suma_total_general = df_cal_pla.get('Aprobado', 0)
-porcentaje_total = (suma_total_general / suma_total) * 100
-df_cal_pla['% Completado'] = porcentaje_total
-# Calcular 'Aprobados' y 'Sin_Enviar' usando get para evitar errores si las columnas no existen
-aprobados = df_cal_pla.get('Aprobado', 0) + df_cal_pla.get('Enviado', 0)
-sin_enviar = df_cal_pla.get('Com. Menores', 0) + df_cal_pla.get('Sin Enviar', 0) + df_cal_pla.get('Com. Mayores', 0) + df_cal_pla.get('Rechazado', 0)
-df_cal_pla['Enviados'] = aprobados
-df_cal_pla['Pendiente'] = sin_enviar
-# Reordenar columnas y filtrar por 'Sin_Enviar' > 0
-df_cal_pla = df_cal_pla[['Nº Pedido', 'Enviados', 'Pendiente', 'Nº Doc. Cliente']]
-df_cal_pla = df_cal_pla[df_cal_pla['Pendiente'] > 0]
-print(df_cal_pla)
-
-# Leer la hoja "DATA" usando pandas para análisis
-df_planos = pd.read_excel(path_to_graphics)
-# Filtrar por 'Tipo Doc.'
-df_planos = df_planos[df_planos['Tipo Doc.'] == 'Planos']
-# Rellenar valores nulos en 'Estado' con 'Sin Enviar'
-df_planos['Estado'] = df_planos['Estado'].fillna('Sin Enviar')
-# Reemplazar NaN en 'Nº Doc. Cliente' con 'Sin TR. Number'
-df_planos['Nº Doc. Cliente'] = df_planos['Nº Doc. Cliente'].fillna('Sin TR. Number')
-# Concatenar 'Nº Doc. Cliente' en caso de duplicados por 'Nº Pedido'
-df_planos['Nº Doc. Cliente'] = df_planos.groupby('Nº Pedido')['Nº Doc. Cliente'].transform(lambda x: ';'.join(x.astype(str).unique()))
-# Eliminar duplicados manteniendo solo la primera ocurrencia de cada 'Nº Pedido'
-df_planos = df_planos.drop_duplicates(subset=['Nº Pedido', 'Estado'])
-# Contar la frecuencia de cada estado por 'Nº Pedido'
-df_planos = df_planos.groupby(['Nº Pedido', 'Nº Doc. Cliente', 'Estado']).size().unstack(fill_value=0).reset_index()
-# Calcular el total y el porcentaje completado
-df_planos['Total'] = df_planos.iloc[:, 2:].sum(axis=1)
-suma_total = df_planos['Total']
-suma_total_general = df_planos.get('Aprobado', 0)
-porcentaje_total = (suma_total_general / suma_total) * 100
-df_planos['% Completado'] = porcentaje_total
-# Calcular 'Aprobados' y 'Sin_Enviar' usando get para evitar errores si las columnas no existen
-aprobados = df_planos.get('Aprobado', 0) + df_planos.get('Enviado', 0)
-sin_enviar = df_planos.get('Com. Menores', 0) + df_planos.get('Sin Enviar', 0) + df_planos.get('Com. Mayores', 0) + df_planos.get('Rechazado', 0)
-df_planos['Enviados'] = aprobados
-df_planos['Pendiente'] = sin_enviar
-# Reordenar columnas y filtrar por 'Sin_Enviar' > 0
-df_planos = df_planos[['Nº Pedido', 'Enviados', 'Pendiente', 'Nº Doc. Cliente']]
-df_planos = df_planos[df_planos['Pendiente'] > 0]
-print(df_planos)
-print("Generando gráficos en la hoja de excel...")
-
 # Reorganizamos las columnas
 df = df.reindex(columns=['Nº Pedido', 'Resp.', 'Nº PO','Cliente', 'Material', 'Nº Doc. Cliente', 'Nº Doc. EIPSA', 'Título', 'Tipo Doc.', 'Crítico', 'Estado', 'Notas','Nº Revisión', 'Fecha', 'Días Devolución', 'Fecha Pedido', 'Fecha Prevista', 'Fecha Contractual', 'Fecha AP VDDL', 'Días VDDL', 'Historial Rev.', 'Seguimiento'])
 df2 = df2.reindex(columns=['Nº Pedido', 'Resp.', 'Nº PO', 'Cliente', 'Material', 'Nº Doc. Cliente', 'Nº Doc. EIPSA', 'Título', 'Tipo Doc.' ,'Crítico', 'Estado', 'Nº Revisión', 'Fecha', 'Fecha Pedido', 'Días Devolución', 'Fecha Prevista',  'Fecha Contractual', 'Fecha AP VDDL', 'Días VDDL', 'Historial Rev.', 'Seguimiento'])
 df3 = critics_no.reindex(columns=['Nº Pedido', 'Resp.', 'Nº PO', 'Cliente', 'Material', 'Nº Doc. Cliente', 'Nº Doc. EIPSA', 'Título', 'Tipo Doc.' , 'Crítico', 'Estado', 'Fecha Pedido', 'Fecha Prevista', 'Fecha Contractual'])
 df4 = critics_si.reindex(columns=['Nº Pedido', 'Resp.', 'Nº PO', 'Cliente', 'Material', 'Nº Doc. Cliente', 'Nº Doc. EIPSA', 'Título', 'Tipo Doc.' , 'Crítico', 'Estado', 'Fecha Pedido', 'Fecha Prevista', 'Fecha Contractual'])
 df6 = df6.reindex(columns=['Nº Pedido', 'Resp.', 'Nº PO', 'Cliente', 'Material', 'Nº Doc. Cliente', 'Nº Doc. EIPSA', 'Título', 'Tipo Doc.' ,'Crítico', 'Estado', 'Nº Revisión', 'Fecha', 'Fecha Pedido', 'Días Devolución', 'Fecha Prevista',  'Fecha Contractual', 'Historial Rev.', 'Seguimiento'])
-df_cal_pla.to_excel('.\\data\\df_cal_pla_' + str(today_date_str) + '.xlsx', index=False)
-df_planos.to_excel('.\\data\\df_planos_' + str(today_date_str) + '.xlsx', index=False)
 print("¡Generando columnas...!")
-
-# Crear un archivo Excel y escribir ambos DataFrames
-output_path = f'./data/merged_data_{today_date_str}.xlsx'
-with pd.ExcelWriter(output_path, engine='xlsxwriter') as writer:
-    # Escribir df_cal_pla en la hoja de cálculo
-    df_cal_pla.to_excel(writer, sheet_name='ESTADO GLOBAL', index=False)
-    # Obtener el objeto workbook y worksheet
-    workbook = writer.book
-    worksheet = writer.sheets['ESTADO GLOBAL']
-    # Escribir df_planos a partir de la columna N
-    startrow = 0
-    startcol = 11  # Columna N es la columna 13 (índice 12 en 0-based indexing)
-    # Escribir encabezados de df_planos a partir de la columna N
-    for c_idx, value in enumerate(df_planos.columns.values):
-        worksheet.write(startrow, startcol + c_idx, value)
-    # Escribir datos de df_planos a partir de la columna N
-    for r_idx, row in enumerate(df_planos.values):
-        for c_idx, value in enumerate(row):
-            worksheet.write(startrow + r_idx + 1, startcol + c_idx, value)
-
-graph_merge = f'./data/merged_data_{today_date_str}.xlsx'
-df_graph_merge = pd.read_excel(graph_merge)
 
 # Seleccionamos las columnas que van a ser coloreadas según el 'ESTADO' que tiene la documentación
 with pd.ExcelWriter(r'C:\\Users\\alejandro.berzal\\Desktop\\DATA SCIENCE\\monitoring_report\\data\\monitoring_report_' + str(today_date_str) + '.xlsx', engine='xlsxwriter') as writer:
@@ -250,34 +190,35 @@ with pd.ExcelWriter(r'C:\\Users\\alejandro.berzal\\Desktop\\DATA SCIENCE\\monito
         highlight_row_content, value="Enviado", color='#B1E1B9', subset=["Estado"], axis=1).apply(
         highlight_row_content, value="Sin Enviar", color='#FFFFAB', subset=["Estado"], axis=1).apply(
         highlight_row_content, value="Aprobado", color='#D4DCF4', subset=["Estado"], axis=1)
-    style_sheet6.to_excel(writer, sheet_name='DOC. TOTAL',index=False)  # Escribir el DataFrame con estilos en la hoja 'pending'
+    style_sheet6.to_excel(writer, sheet_name='ALL DOC.',index=False)  # Escribir el DataFrame con estilos en la hoja 'pending'
+    style_sheet_2 = df2.style.apply(highlight_row_content, value="Enviado", color='#B1E1B9', subset=["Estado"], axis=1) # Aplicar estilos al DataFrame 'df_under_review'
+    style_sheet_2.to_excel(writer, sheet_name='ENVIADOS', index=False) # Escribir el DataFrame con estilos en la hoja 'df_under_review'
+    style_sheet_3 = df3.style.apply(highlight_row_content, value='Sin Enviar', color='#FFFFAB', subset=["Estado"], axis=1) # Aplicar estilos al DataFrame 'df_to_upload'
+    style_sheet_3.to_excel(writer, sheet_name='SIN ENVIAR', index=False) # Escribir el DataFrame con estilos en la hoja 'to_upload'
     style_sheet = df.style.apply(
         highlight_row_content, value="Rechazado", color='#FFA19A', subset=["Estado", "Notas"], axis=1).apply(
         highlight_row_content, value="Com. Menores", color='#FFE5AD', subset=["Estado", "Notas"], axis=1).apply(
         highlight_row_content, value="Com. Mayores", color='#DBB054', subset=["Estado", "Notas"], axis=1).apply(
         highlight_row_content, value="Comentado", color='#F79646', subset=["Estado", "Notas"], axis=1)
-    style_sheet.to_excel(writer, sheet_name='DOC. COMENTARIOS', index=False) # Escribir el DataFrame con estilos en la hoja 'pending'
-    style_sheet_2 = df2.style.apply(highlight_row_content, value="Enviado", color='#B1E1B9', subset=["Estado"], axis=1) # Aplicar estilos al DataFrame 'df_under_review'
-    style_sheet_2.to_excel(writer, sheet_name='DOC. ENVIADA', index=False) # Escribir el DataFrame con estilos en la hoja 'df_under_review'
-    style_sheet_3 = df3.style.apply(highlight_row_content, value='Sin Enviar', color='#FFFFAB', subset=["Estado"], axis=1) # Aplicar estilos al DataFrame 'df_to_upload'
-    style_sheet_3.to_excel(writer, sheet_name='DOC. SIN ENVIAR', index=False) # Escribir el DataFrame con estilos en la hoja 'to_upload'
-    style_sheet_4 = df4.style.apply(highlight_row_content, value='Sin Enviar', color='#FFFFAB', subset=["Estado"], axis=1).apply(highlight_row_content, value=" ", color='#FFFFAB', subset=["Estado"], axis=1) # Aplicar estilos al DataFrame 'df_to_upload'
+    style_sheet.to_excel(writer, sheet_name='COMENTADOS', index=False) # Escribir el DataFrame con estilos en la hoja 'pending'
+    style_sheet_4 = df4.style.apply(highlight_row_content, value="Rechazado", color='#FFA19A', subset=["Estado"], axis=1).apply(
+        highlight_row_content, value="Com. Menores", color='#FFE5AD', subset=["Estado"], axis=1).apply(
+        highlight_row_content, value="Com. Mayores", color='#DBB054', subset=["Estado"], axis=1).apply(
+        highlight_row_content, value="Comentado", color='#F79646', subset=["Estado"], axis=1).apply(
+        highlight_row_content, value="Sin Enviar", color='#FFFFAB', subset=["Estado"], axis=1) # Aplicar estilos al DataFrame 'df_to_upload'
     style_sheet_4.to_excel(writer, sheet_name='CRÍTICOS', index=False) # Escribir el DataFrame con estilos en la hoja 'to_upload'
-    df_graph_merge.to_excel(writer, sheet_name='GRÁFICO CRÍTICOS', index=False)
-    df5.to_excel(writer, sheet_name='ESTADO GLOBAL', index=False) # Escribir el DataFrame con estilos en la hoja 'pending'
+    df5.to_excel(writer, sheet_name='STATUS GLOBAL', index=False) # Escribir el DataFrame con estilos en la hoja 'pending'
 print("¡Estilo, formato y color aplicado correctamente a todas las hojas del excel!")
-
 
 # Cargar archivo de Excel con las tres hojas de datos
 wb = Workbook('C:\\Users\\alejandro.berzal\\Desktop\\DATA SCIENCE\\monitoring_report\\data\\monitoring_report_' + today_date_str + '.xlsx')
 # Obtener la referencia de las hojas/sheets de trabajo deseadas
-sheets = {"DOC. TOTAL": wb.getWorksheets().get("DOC. TOTAL"),
-          "DOC. COMENTARIOS": wb.getWorksheets().get("DOC. COMENTARIOS"),
-          "DOC. ENVIADA": wb.getWorksheets().get("DOC. ENVIADA"),
-          "DOC. SIN ENVIAR": wb.getWorksheets().get("DOC. SIN ENVIAR"),
+sheets = {"ALL DOC.": wb.getWorksheets().get("ALL DOC."),
+          "COMENTADOS": wb.getWorksheets().get("COMENTADOS"),
+          "ENVIADOS": wb.getWorksheets().get("ENVIADOS"),
+          "SIN ENVIAR": wb.getWorksheets().get("SIN ENVIAR"),
           "CRÍTICOS": wb.getWorksheets().get("CRÍTICOS"),
-          "GRÁFICO CRÍTICOS": wb.getWorksheets().get("GRÁFICO CRÍTICOS"),
-          "ESTADO GLOBAL":wb.getWorksheets().get("ESTADO GLOBAL")}
+          "STATUS GLOBAL":wb.getWorksheets().get("STATUS GLOBAL")}
 
 # Ajuste automático de todas las columnas en cada hoja
 for sheet_name, sheet in sheets.items():
--- tools/apply_style_mr.py
+++ tools/apply_style_mr.py
@@ -12,20 +12,17 @@ def highlight_row_content(column, value, color):
     cont_val = column == value
     return [f'background-color: {color}' if cont_val.any() else '' for v in cont_val]
 
-
 # Función para definir el rango de las celdas
 def auto_fit_columns(sheet):
     if sheet:
         for col_index in range(sheet.getCells().getMaxDataColumn() + 1):
             sheet.autoFitColumn(col_index)
 
-
 # Coloreado y estilos de la tabla de excel
 def apply_excel_styles(today_date):
     # Carga el archivo de Excel existente
     archivo_excel = "Monitoring_Report_" + str(today_date) + ".xlsx"
     workbook = load_workbook(archivo_excel)
-
     # Definir los estilos
     fecha_style = NamedStyle(name='fecha')
     fecha_style.number_format = 'DD-MM-YYYY'
@@ -38,13 +35,11 @@ def apply_excel_styles(today_date):
                            bottom=Side(style='thin'))
     font_white = Font(color='FFFFFF', bold=True)
     font_black = Font(color='000000')
-
     # Definir el estilo condicional para fechas de pedido superiores a las fechas previstas
     red_fill = PatternFill(start_color="FF5B5B", end_color="FF5B5B", fill_type="solid")
     diff_style = DifferentialStyle(fill=red_fill)
     rule = Rule(type="expression", dxf=diff_style)
     rule.formula = ["$A2>$B2"]  # Ajustar según las columnas de fechas
-
     # Definir el estilo condicional para días de devolución >= 15
     red_fill_2 = Font(color="FF5B5B", bold=True)
     diff_style_devolucion = DifferentialStyle(font=red_fill_2)
@@ -98,19 +93,15 @@ def apply_excel_styles(today_date):
                     celda.font = Font(color='FF5B5B', bold=True)
                 if celda.value == 100:
                     celda.font = Font(color='FF5B5B', bold=True)
-
+        # Autofiltro de columnas
         sheet.auto_filter.ref = f"A1:{chr(65 + max_col - 1)}{max_row}"
-
-        # Aplicar la regla de formato condicional fechas pedidos y prevista
-        #sheet.conditional_formatting.add(f"P2:Q2{max_row}", rule)
         # Definir el estilo condicional para días de devolución >= 15
         sheet.conditional_formatting.add(f"O2:O{max_row}", rule_devolucion)
-
 
     def add_chart(sheet):
         chart = BarChart()
         chart.type = "col"  # Configurar el gráfico como gráfico de columnas
-        chart.title = "Estado de la Documentación (PENDIENTES)"
+        chart.title = "Estado de la Documentación (Pendientes)"
         chart.y_axis.title = 'PORCENTAJE COMPLETADO'
         chart.x_axis.title = 'Nº DE PEDIDOS'
         chart.style = 10
@@ -124,6 +115,7 @@ def apply_excel_styles(today_date):
                 pedido = sheet.cell(row=row, column=1).value
                 filtered_categories.append(pedido)
                 filtered_data.append(porcentaje_completado)
+
         # Escribimos las categorías y datos filtrados en columnas temporales al final de la hoja
         start_col = sheet.max_column + 20
         for idx, value in enumerate(filtered_categories, start=2):
@@ -145,133 +137,15 @@ def apply_excel_styles(today_date):
             cell = sheet[f'{col}1']
             cell.fill = PatternFill(fill_type=None)
 
-    def add_stacked_bar_chart_cal_pla(sheet):
-        chart = BarChart()
-        chart.type = "col"
-        chart.title = "Estado Doc. Cálculos y Planos (ESP-0003)"
-        chart.style = 10
-        chart.y_axis.title = 'DOCUMENTOS'
-        chart.x_axis.title = 'Nº DE PEDIDO'
-        chart.varyColors = "0000FFFF"
-
-        # Encontrar la fila que contiene los datos
-        min_row = None
-        for row in sheet.iter_rows():
-            if min_row is None:
-                for cell in row:
-                    if cell.value is not None:
-                        min_row = row[2].row
-                        break
-
-        # Si no se encontraron datos, salir de la función
-        if min_row is None:
-            print("No se encontraron datos en la hoja.")
-            return
-
-        max_row = sheet.max_row
-        max_col = 3
-
-        # Determinar el rango de columnas
-        min_col = 2  # Empezamos desde la segunda columna (suponiendo que la primera contiene etiquetas)
-        for cell in sheet[min_row]:
-            if cell.value is not None:
-                break
-            min_col += 1
-
-        # Seleccionar los datos y categorías
-        data = Reference(sheet, min_col=min_col, min_row=min_row, max_row=max_row, max_col=max_col)
-        categories = Reference(sheet, min_col=1, min_row=2, max_row=max_row, max_col=1)
-
-        chart.add_data(data, titles_from_data=True)
-        chart.set_categories(categories)
-
-        # Habilitar etiquetas de datos
-        chart.dataLabels = DataLabelList()
-        chart.dataLabels.showVal = True
-
-        # Posicionar y escalar el gráfico
-        sheet.add_chart(chart, "E1")
-        chart.width = 15
-        chart.height = 10
-
-        for col in ['K', 'L', 'J']:
-            cell = sheet[f'{col}1']
-            cell.fill = PatternFill(fill_type=None)
-
-    def add_stacked_bar_chart_planos(sheet):
-        chart = BarChart()
-        chart.type = "col"
-        chart.title = "Estado Doc. Planos (PLG-0005)"
-        chart.style = 10
-        chart.y_axis.title = 'DOCUMENTOS'
-        chart.x_axis.title = 'Nº DE PEDIDO'
-        chart.varyColors = "0000FFFF"
-
-        # Encontrar la fila que contiene los datos
-        min_row = None
-        for row in sheet.iter_rows():
-            if min_row is None:
-                for cell in row:
-                    if cell.value is not None:
-                        min_row = row[2].row
-                        break
-
-        # Si no se encontraron datos, salir de la función
-        if min_row is None:
-            print("No se encontraron datos en la hoja.")
-            return
-
-        max_row = sheet.max_row
-        max_col = 14
-
-        # Determinar el rango de columnas
-        min_col = 13  # Empezamos desde la segunda columna (suponiendo que la primera contiene etiquetas)
-        for cell in sheet[min_row]:
-            if cell.value is not None:
-                break
-            min_col += 1
-
-        # Seleccionar los datos y categorías
-        data = Reference(sheet, min_col=min_col, min_row=min_row, max_row=max_row, max_col=max_col)
-        categories = Reference(sheet, min_col=12, min_row=2, max_row=max_row, max_col=12)
-
-        chart.add_data(data, titles_from_data=True)
-        chart.set_categories(categories)
-
-        # Habilitar etiquetas de datos
-        chart.dataLabels = DataLabelList()
-        chart.dataLabels.showVal = True
-
-        # Posicionar y escalar el gráfico
-        sheet.add_chart(chart, "P1")
-        chart.width = 15
-        chart.height = 10
-
-        # Iterar a través de cada columna y fila para aplicar los cambios
-        start_row = 1
-        end_row = 10
-        for col in ['E', 'F', 'G', 'H', 'I', 'J', 'K',]:
-            for row in range(start_row, end_row + 1):
-                cell = sheet[f'{col}{row}']
-                cell.fill = PatternFill(fill_type=None)  # Eliminar el relleno
-                cell.border = Border()  # Eliminar los bordes
-        cell_letters = sheet['L1']
-        cell_letters.fill = cell_filling
-
     # Aplicar estilos a cada hoja
-    apply_styles_to_sheet(workbook['DOC. COMENTARIOS'], "DBB054", 200, 21, ('K', 'L'))
-    apply_styles_to_sheet(workbook['DOC. ENVIADA'], "B1E1B9", 200, 20, ('K'))
-    apply_styles_to_sheet(workbook['DOC. SIN ENVIAR'], "DDDDDD", 200, 13, ('K'))
+    apply_styles_to_sheet(workbook['ALL DOC.'], "0072C8", 500, 18, ('K'))
+    apply_styles_to_sheet(workbook['ENVIADOS'], "B1E1B9", 200, 20, ('K'))
+    apply_styles_to_sheet(workbook['SIN ENVIAR'], "DDDDDD", 200, 13, ('K'))
+    apply_styles_to_sheet(workbook['COMENTADOS'], "DBB054", 200, 21, ('K', 'L'))
     apply_styles_to_sheet(workbook['CRÍTICOS'], "FFFF46", 200, 13, ('K'))
-    grafico_sheet = workbook['ESTADO GLOBAL']
-    apply_styles_to_sheet(workbook['DOC. TOTAL'], "99CCFF", 500, 18, ('K'))
+    grafico_sheet = workbook['STATUS GLOBAL']
     apply_styles_to_sheet(grafico_sheet, "FFAAAB", 110, 10, ('K','L','M'))
     add_chart(grafico_sheet)
-    grafico_planos = workbook['GRÁFICO CRÍTICOS']
-    apply_styles_to_sheet(grafico_planos, "FFFFAB", 2, 3)
-    add_stacked_bar_chart_cal_pla(grafico_planos)
-    grafico_planos = workbook['GRÁFICO CRÍTICOS']
-    add_stacked_bar_chart_planos(grafico_planos)
     # Guardar el archivo modificado
     workbook.save(archivo_excel)
     print("¡Creando los filtros de las columnas!")
